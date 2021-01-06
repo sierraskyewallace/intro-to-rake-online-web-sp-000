ca3b685b53dbe994e96c0aceaa79909d892dc501
Sierra Wallace <sskwallace@gmail.com>
2021-01-06 22:23:25 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,3 @@ Student.create(name: "Devon", grade: "11th")
 Student.create(name: "Sarah", grade: "10th")
 
 
-namespace :db do
- 
-  ...
- 
-  desc 'seed the database with some dummy data'
-  task :seed do
-    require_relative './db/seeds.rb'
-  end
-end
